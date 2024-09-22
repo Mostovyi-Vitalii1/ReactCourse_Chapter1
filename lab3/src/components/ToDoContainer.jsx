93ce2eb9e1ae4e230aa090daa8798715cd2110d9
Mostovyi-Vitalii1 <163344368+Mostovyi-Vitalii1@users.noreply.github.com>
2024-09-22 21:18:29 +0300

    Update-lab-3

--- lab3/src/components/ToDoContainer.jsx
+++ lab3/src/components/ToDoContainer.jsx
@@ -1,14 +1,32 @@
-import React from 'react';
-import Add from './Add';
+import React, { useState } from 'react';
+import AddTodo from './AddTodo';
 import Search from './Search';
 import Table from './Table';
 
-const ToDoContainer = ({ newTodo, setNewTodo, addTodo, search, setSearch, todos, removeTodo }) => {
+const ToDoContainer = ({ todos, setTodos }) => {
+  const [newTodo, setNewTodo] = useState('');
+  const [search, setSearch] = useState('');
+
+  const addTodo = () => {
+    if (newTodo.trim() === '') return;
+    const newId = todos.length > 0 ? todos[todos.length - 1].id + 1 : 1;
+    setTodos([...todos, { id: newId, title: newTodo }]);
+    setNewTodo('');
+  };
+
+  const removeTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  const filteredTodos = todos.filter((todo) =>
+    todo.title.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
     <div>
-      <Add newTodo={newTodo} setNewTodo={setNewTodo} addTodo={addTodo} />
+      <AddTodo newTodo={newTodo} setNewTodo={setNewTodo} addTodo={addTodo} />
       <Search search={search} setSearch={setSearch} />
-      <Table todos={todos} removeTodo={removeTodo} />
+      <Table todos={filteredTodos} removeTodo={removeTodo} />
     </div>
   );
 };
