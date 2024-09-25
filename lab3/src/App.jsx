9a7842a7c759ef666efb7ca01360cc89c2e40e8f
Mostovyi-Vitalii1 <163344368+Mostovyi-Vitalii1@users.noreply.github.com>
2024-09-25 15:02:49 +0300

    1

--- lab3/src/App.jsx
+++ lab3/src/App.jsx
@@ -4,15 +4,10 @@ import PageTitle from './components/PageTitle';
 import ToDoContainer from './components/ToDoContainer';
 
 const App = () => {
-  const [todos, setTodos] = useState([
-    { id: 1, title: 'delectus aut autem' },
-    { id: 2, title: 'quis ut nam facilis et officia qui' },
-  ]);
-
   return (
     <div className="App">
       <PageTitle title="ToDo App." />
-      <ToDoContainer todos={todos} setTodos={setTodos} />
+      <ToDoContainer/>
     </div>
   );
 };
